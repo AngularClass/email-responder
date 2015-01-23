ae29f3b1e61bf9fdc78b3d473467182fbce132f9
Hendrixer <willscottmoss@gmail.com>
2015-01-22 23:12:37 -0800

    init

--- config/globals.js
+++ config/globals.js
@@ -1,14 +1,12 @@
 'use strict';
-
+var fs = require('fs');
 
 global._ = require('lodash');
 global.$handleError = function(message, error){};
+global.$whiteList = JSON.parse(fs.readFileSync(__dirname + '/whitelist.json', 'utf-8'));
 
 global.$config = require('./env');
-
-if ($config.logging) {
-  global.$log = console;
-}
+global.$log = $config.logging ? console : noop;
 
 
 function noop(){};
