bc5c14cfeec0ddb4d473e8690c478f58926681a7
Hendrixer <willscottmoss@gmail.com>
2015-01-25 00:37:14 -0800

    rename file for cache

--- components/mailer/templates/index.js
+++ components/mailer/templates/index.js
@@ -4,6 +4,6 @@ var path = require('path');
 var templates = module.exports = {};
 
 templates.angularclass = {
-  subscribeBody: fs.readFileSync(path.join(__dirname, 'ngClass_subscribe.html'), 'utf-8'),
+  subscribeBody: fs.readFileSync(path.join(__dirname, 'ngClassSubscribe.html'), 'utf-8'),
   subscribeSubject: 'Get ready to be a ng-enius {{ name }}'
 };
